d7b90e47d67329b26d43309766f3df1a24e26cce
immort01 <leaptics@gmail.com>
2025-03-02 16:09:23 +0530

    TIMETABLE.JS

--- script.js
+++ script.js
@@ -12,33 +12,34 @@ switch(dayNumber){
 
     case 0:
         theDayIs = "Sunday";
-        quote = "Time to chillax"
+        quote = "Time to WORK HARD AND PLAN FOR NEXT WEEK I AM THE BEST  :)"
+        
         break;
 
     case 1:
         theDayIs = "Monday";
-        quote = "Monday is a day of Work";
+        quote = "SMC(9-10),Hss(10-11),HSS LAB(11-1:30),PY (2:30-3:30),PY LAB(3:30-6:00)";
         break;
 
     case 2:
         theDayIs = "Tuesday";
-        quote = "Tuesday is a Second day of Work";
+        quote = "DLD LAB(9-11),MATH(11-12),DSA(12-1:30),ENG(2:30-4:30),DLD(4:30-6:00)";
         break;
     
     case 3:
         theDayIs = "Wednesday";
-        quote = "Wednesday is a Third day of Work";
+        quote = "WD LAB(9-11),MATH(11-12),MATH TUT(12-1:30), :)";
         break;
             
             
     case 4:
         theDayIs = "Thursday";
-        quote = "Thursday is a Meeting day of Work";
+        quote = "WD LAB(9-11),MATH(11-12),DS AB (12-1:30),DS LAB(2:30-5:30),PY :)";
         break;
         
     case 5:
         theDayIs = "Friday";
-        quote = "Weekend is almost here.....";
+        quote = "CS RR(10-11) MATH (11-12) PY(12-1:30) ENG(2:30-4:30) DLD(6PM)";
         break;
         
     case 6:
